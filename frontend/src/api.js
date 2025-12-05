745ba65d573d9962a2d1873b18a02aa0bef9e04a
oliveirafjdo-web <oliveirafjdo@gmail.com>
2025-12-05 16:26:22 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,13 @@
 export async function fetchJson(url, options = {}) {
   try {
     const res = await fetch(url, options);
+
     if (!res.ok) {
       throw new Error(Erro HTTP ${res.status});
     }
+
     return await res.json();
+
   } catch (err) {
     console.error("Erro no fetchJson:", err);
     throw err;
